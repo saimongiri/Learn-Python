e52a4c545ddce846c382f74b335da1b00a155538
Saimon <saimongiri401@gmail.com>
2026-05-10 22:03:38 +0545

    Arithimatic and assignment operators

---
+++ Basic/operators.py
@@ -0,0 +1,34 @@
+x = 5
+y = 10
+
+# Arithimetic Operators
+print("Addition : ",x+y)
+print("Subtraction : ",x-y)
+print("Multiplication : ",x*y)
+print("Division : ",float(x/y))
+print("Modulus : ",x%y)
+print("Exponential : ",x**y)
+print("Floor Division : ",x//y)
+
+# Assignment Operator
+
+a=3 # assign value to a
+print("a=3",a)
+a+=3 # a=a+3
+print("a+=3 : ",a)
+a-=3 # a=a-3
+print("a-=3 : ",a)
+
+a*=3 # a=a*3
+print("a*=3 : ",a)
+a/=3 # a=a/3
+print("a/=3 : ",a)
+
+a%=3 # a=a+3
+print("a%=3 : ",a)
+a//=3 # a=a//3
+print("a//=3 : ",a)
+
+a**=3 # a=a**3
+print("a**=3 : ",a)
+
