d330abd3153006164966d2850c10e0e67c5c8a80
Saimon <saimongiri401@gmail.com>
2026-05-10 21:08:13 +0545

    Python variable and type casting

---
+++ Basic/basic.py
@@ -0,0 +1,28 @@
+# This is used for coding of basic python
+
+print(" Hello World!")
+
+# Variables (Python has no command for declaring a variabel)
+
+x=5 # It automatically detect it as the integer variable
+print(type(x)) # type function is used for getting type of variables, list or other datasets
+
+y = "John" # y is string
+
+z = 0.10 # z is float
+
+# Casting 
+
+x = str(3) # x will be string '3'
+y= int(3)
+z= float(3)
+
+print(x , y , z)
+
+# Variable names are case sensitive
+
+a = 4
+A = "Saimon"
+# A will not overwrite a
+print( a , A)
+
