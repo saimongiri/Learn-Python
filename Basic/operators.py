b9687bd9d97e5a488e47db71917234f9aea79c6b
Saimon <saimongiri401@gmail.com>
2026-05-10 22:09:50 +0545

    Conditional Operator

--- Basic/operators.py
+++ Basic/operators.py
@@ -32,3 +32,21 @@ print("a//=3 : ",a)
 a**=3 # a=a**3
 print("a**=3 : ",a)
 
+
+# Comparison Operator
+
+x = int(input("Enter first number : "))
+y = int(input("Enter another number :"))
+
+if x==y: #equal
+    print("X and Y are equals")
+if x!=y : # not equal
+    print("x and y are not equal")
+if x>y : # greater than
+    print("x is greater than y")
+if x<y : #less than
+    print("y is greater than x")
+if x>=y: # greater than and equal
+    print(" X is greater or equal to y")
+if x<=y: # less than or equal
+    print("y is less than or equal to x")
