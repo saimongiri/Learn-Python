430e7eb85566877d3bd7109478604fff8c18d92a
Saimon <saimongiri401@gmail.com>
2026-05-10 22:21:58 +0545

    List some operations

---
+++ Basic/datastore.py
@@ -0,0 +1,17 @@
+# List
+
+mylist = ['apple' , 'banana' , 'Cherry'] # list are changeable and allows duplicates
+print(mylist)
+ 
+ # List items are indexed from 0
+
+ #list length
+ print(len(mylist))
+
+# list type
+print(type(mylist))
+
+# List Constructor
+
+thislist = list(("mango","Orange"))#note the double rounded brackets
+print(thislist)
