72b95ba1381be5711f5d7665305710a8f69bd9f9
Saimon <saimongiri401@gmail.com>
2026-05-11 18:05:13 +0545

    Basic of function creation

---
+++ Basic/function.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello from the function")
+
+
+greet()
+
+def add(a , b):
+    return a+b
+
+sum = add(3,4)
+print(sum)
+
+
+def sayHi():
+    pass # pass is used for future use of function
