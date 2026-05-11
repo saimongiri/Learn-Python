eb6cc963151f144536ed939108879c06b57f555a
Saimon <saimongiri401@gmail.com>
2026-05-11 17:57:03 +0545

    Switch case in python

---
+++ Basic/match.py
@@ -0,0 +1,17 @@
+day = 4
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("tuesday")
+    case 3:
+        print("wednesday")
+    case 4:
+        print("thursday")
+    case 5:
+        print("Friday")
+    case 6 :
+        print("Saturday")
+    case 7: 
+        print("sunday")
+    
